badcda4decc1c826f5a237d4add8cf9ff35f08f1
Adamskid9 <ajaniadam9@gmail.com>
2023-10-25 00:49:25 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <div className={"name"} style={{display:"flex",flexDirection:"column"}}>
+    <span style={{fontSize:"3rem"}}>hello world 3rem</span>
+    <span style={{fontSize:"2rem"}}>hello world 2rem</span>
+    <span style={{fontSize:"1rem"}}>hello world 1rem</span>
+    <span style={{fontSize:".8rem"}}>hello world .8rem</span>
+    <span style={{fontSize:".5rem"}}>hello world .8rem</span>
+
+    <span className="love">i Love you Zahra</span>
+    </div>
+  )
+}
+
+export default App
