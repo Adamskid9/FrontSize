75823a32541eb8d1c33e7df9d924f13768b23b77
Adamskid9 <ajaniadam9@gmail.com>
2023-11-30 23:17:33 +0100

    one

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 import {Toaster} from "react-hot-toast"
 
 import Test from "./ui/Test"
-import Footer from "./ui/footer"
+import Footer from "./ui/Footer"
 
 
 const queryClient = new QueryClient({
@@ -22,7 +22,28 @@ function App() {
   return <BrowserRouter>
   <QueryClientProvider client={queryClient}>
   <ReactQueryDevtools initialIsOpen={false} />
-  <Toaster gutter={12} position="top" />
+  <Toaster gutter={12} 
+   position="top-center"
+   containerStyle={{margin:"8px"}}
+   toastOptions={{
+    success:{
+      duration:3000,
+    },
+    error:{
+      duration:5000,
+      color:"red"
+    },
+    style:{
+      fontSize:"16px",
+      maxWidth:"500px",
+      padding:"16px 24px" ,
+      backgroundColor:"blue",
+      color:"white",
+      borderRadius:"100%"
+    }
+   }}
+
+    />
   <Routes>
     <Route index element={<Test/>}/>
     <Route path="test" element={<Header/>}/>
--- src/ui/Portal.jsx
+++ src/ui/Portal.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react"
+import toast from "react-hot-toast";
 
 function Portal() {
     const [isOpen,setIsOpen] = useState(false);
-  
+     const  conditional = isOpen?`duration-500 hover:bg-gradient-to-tr hover:from-red-500
+     hover:to-yellow-500 hover:bg-[length:100%] relative  bg-[length:30%] 
+     hover:shadow-black shadow-md shadow-orange-500`:""
 
 
     return (<>
-        <button onClick={()=>setIsOpen(cur=>!cur)} className="font-bold flex transition-all  duration-500 hover:bg-gradient-to-tr hover:from-red-500
-         hover:to-yellow-500 hover:bg-[length:100%] relative  bg-[length:30%] 
-         
-         hover:shadow-black shadow-md shadow-orange-500 cursor-pointer text-white justify-center
-          items-center py-3 uppercase bg-blue-600 w-2/12">
+        <button onClick={()=>setIsOpen(cur=>!cur)} className={`font-bold flex transition-all cursor-pointer
+         text-white justify-center ${conditional}
+          items-center py-3 uppercase bg-blue-600 w-2/12 `}>
            {isOpen?"close":"open"}           
-        </button>
          {isOpen&&<Modal/>}
+        </button>
         </>
     )
 }
@@ -24,8 +25,10 @@ export default Portal
 
 function Modal(){
 
-    return ( <div className="bg-black p-2 text-md font-medium text-white 
-     left-[100px] z-50 absolute">
+   
+    return ( <div onClick={()=>toast.success("hello world")} className="bg-black p-2 text-md font-medium text-white 
+     left-0 z-50">
+        <span></span>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, sapiente.
          Officiis id quam necessitatibus pariatur aut praesentium dicta rerum modi.
     </div>
